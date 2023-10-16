47c0aa7843e1252a432a4dac6690671e7236868c
jbjzeehad <ilamta.97@gmail.com>
2023-10-16 21:34:09 +0600

    fixed blue badge and publish time

--- phtube.js
+++ phtube.js
@@ -13,41 +13,51 @@ const loadpage = async (searchcategory) => {
     }
 };
 const phTubeVideoCard = phVideos => {
+    // cheking the views
+    const a = [];
     const videoShowCard = document.getElementById('videoCard');
     videoShowCard.textContent = "";
     phVideos.forEach(showvideo => {
+        const vdview = showvideo.others.views;
+        console.log(vdview);
+        a.push(vdview.replace("K", ""));
         const publishtime = showvideo.others.posted_date;
-        console.log(typeof (publishtime));
-        // convert second to day hours minute
         const postedDate = convertsecondtodhm(publishtime);
         console.log(postedDate);
+        let num = 0;
+        if (showvideo.authors[0].verified == true) {
+            num = 1;
+        }
+
 
         const videocard = document.createElement('div');
-        videocard.classList = `card card-compact border grid grid-rows-1`;
+        videocard.classList = `border card card-compact grid grid-rows-1`;
         videocard.innerHTML = `
-        <div class="1 px-3">
-            <figure><img  src="${showvideo.thumbnail}" alt="videoThumnail"/></figure>
+        <div class="relative px-3">
+            <img src="${showvideo.thumbnail}">${postedDate}
         </div>
         <div class="grid card-body grid-cols-5">
             <div class =" prfl-img ">
-                <img class=" rounded-full w-8 h-8" src="${showvideo.authors[0].profile_picture}" alt="prf" />
+                <img class=" rounded-full w-7 h-7" src="${showvideo.authors[0].profile_picture}" alt="prf" />
             </div>
-            <div class=" txt-cont col-span-4">
-                <h2 class=" card-title text-base text-black font-bold">${showvideo.title}</h2>
-                        <p class=" col-span-2 text-sm font-normal">${showvideo.authors[0].profile_name} ${showvideo.authors[0].verified}<img class="w-3" src="icon2.png"/></p>
+            <div class="col-span-4">
+                <h2 class="card-title text-base text-black font-bold">${showvideo.title}</h2>
+                <p class="text-sm font-normal flex gap-2">${showvideo.authors[0].profile_name}${bluebadge(num)}
+                </p>    
                 <p>${showvideo.others.views} views</p>
             </div>
         </div>
     `;
         videoShowCard.appendChild(videocard);
     })
+    console.log(a);
 };
 const noDatafound = phVideos => {
     const videoShowCard = document.getElementById('videoCard');
     videoShowCard.textContent = "";
     // console.log(showvideo);
     const videocard = document.createElement('div');
-    videocard.classList = `grid col-span-4  justify-items-center p-5 my-16`;
+    videocard.classList = `grid col-span-4  justify-items-center p-4 my-16`;
     videocard.innerHTML = `
         <img class="w-28 " src="Icon.png" alt="">
         <p class='font-bold text-center text-xl md:text-2xl lg:text-3xl text-neutral-900 p-1 md:p-2 lg:p-3'>Oops!! Sorry, There is no content here</p>
@@ -55,18 +65,15 @@ const noDatafound = phVideos => {
     videoShowCard.appendChild(videocard);
 };
 
-// const videoCategory = async () => {
-//     const read = await fetch('https://openapi.programming-hero.com/api/videos/categories');
-//     const data = await read.json();
-//     const cat = data.data;
-//     console.log(cat);
-// }
-
-// const selectCategory = vidCat => {
-//     vidCat.forEach(c => {
-//         if ()
-//     })
-// }
+function bluebadge(value) {
+    const badge = value;
+    if (badge === 1) {
+        return `<img class="w-4 h-4" src="https://img.icons8.com/color/48/instagram-verification-badge.png"/>`
+    }
+    else {
+        return "";
+    }
+}
 
 function convertsecondtodhm(seconds) {
     const second = parseInt(seconds);
@@ -79,22 +86,23 @@ function convertsecondtodhm(seconds) {
     const minute = Math.floor(((second % 86400) % 3600) / 60);
     console.log(minute);
     if (day > 0) {
-        return `${day}day ${hour}hrs ${minute} min ago`;
+        const pubtime = `${day}day ${hour}hrs ${minute} min ago`;
+        return `<div class="thumbtext p-1 absolute rounded-md text-xs bg-neutral-900">${pubtime}</div>`
 
     }
     else if (hour > 0) {
-        return `${hour}hrs ${minute} min ago`;
+        const pubtime = `${hour}hrs ${minute} min ago`;
+        return `<div class="thumbtext p-1 absolute rounded-md text-xs bg-neutral-900">${pubtime}</div>`
 
     }
     else if (minute > 0) {
-        return `${minute} min ago`;
+        const pubtime = `${minute} min ago`;
+        return `<div class="thumbtext p-1 absolute rounded-md text-xs bg-neutral-900">${pubtime}</div>`
     }
     else {
-        return "";
+        return '';
     }
 }
-
-
 
 
 function categorybutton(catid) {
@@ -102,11 +110,6 @@ function categorybutton(catid) {
 }
 loadpage('1000');
 
-
-
-// videoCategory();
-
-// loadpage();
 
 
 
