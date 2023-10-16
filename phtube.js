205b4b91baafc67969ca021c3c6c7f332874e4ab
jbjzeehad <ilamta.97@gmail.com>
2023-10-17 02:16:51 +0600

    blog page and page responsive done

--- phtube.js
+++ phtube.js
@@ -3,7 +3,6 @@ const loadpage = async (searchcategory) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${searchcategory}`);
     const data = await response.json();
     const phdata = data.data;
-    console.log(data.data);
     if (phdata == false) {
         console.log('No data found');
         noDatafound(phdata);
@@ -13,7 +12,7 @@ const loadpage = async (searchcategory) => {
     }
 };
 const phTubeVideoCard = phVideos => {
-    // cheking the views
+    // checking the views
     const a = [];
     const videoShowCard = document.getElementById('videoCard');
     videoShowCard.textContent = "";
@@ -23,7 +22,6 @@ const phTubeVideoCard = phVideos => {
         a.push(vdview.replace("K", ""));
         const publishtime = showvideo.others.posted_date;
         const postedDate = convertsecondtodhm(publishtime);
-        console.log(postedDate);
         let num = 0;
         if (showvideo.authors[0].verified == true) {
             num = 1;
@@ -31,14 +29,14 @@ const phTubeVideoCard = phVideos => {
 
 
         const videocard = document.createElement('div');
-        videocard.classList = `border card card-compact grid grid-rows-1`;
+        videocard.classList = ` card card-compact grid grid-rows-1 gap-1`;
         videocard.innerHTML = `
         <div class="relative px-3">
             <img src="${showvideo.thumbnail}">${postedDate}
         </div>
         <div class="grid card-body grid-cols-5">
-            <div class =" prfl-img ">
-                <img class=" rounded-full w-7 h-7" src="${showvideo.authors[0].profile_picture}" alt="prf" />
+            <div>
+                <img class=" rounded-full w-8 h-8" src="${showvideo.authors[0].profile_picture}" alt="prf" />
             </div>
             <div class="col-span-4">
                 <h2 class="card-title text-base text-black font-bold">${showvideo.title}</h2>
@@ -55,11 +53,10 @@ const phTubeVideoCard = phVideos => {
 const noDatafound = phVideos => {
     const videoShowCard = document.getElementById('videoCard');
     videoShowCard.textContent = "";
-    // console.log(showvideo);
     const videocard = document.createElement('div');
     videocard.classList = `grid col-span-4  justify-items-center p-4 my-16`;
     videocard.innerHTML = `
-        <img class="w-28 " src="Icon.png" alt="">
+        <img class="w-28 " src="others/Icon.png" alt="">
         <p class='font-bold text-center text-xl md:text-2xl lg:text-3xl text-neutral-900 p-1 md:p-2 lg:p-3'>Oops!! Sorry, There is no content here</p>
     `;
     videoShowCard.appendChild(videocard);
@@ -77,23 +74,16 @@ function bluebadge(value) {
 
 function convertsecondtodhm(seconds) {
     const second = parseInt(seconds);
-    // console.log(second);
-    // console.log(typeof (second));
     const day = Math.floor(second / 86400);
-    console.log(day);
     const hour = Math.floor((second % 86400) / 3600);
-    console.log(hour);
     const minute = Math.floor(((second % 86400) % 3600) / 60);
-    console.log(minute);
     if (day > 0) {
         const pubtime = `${day}day ${hour}hrs ${minute} min ago`;
         return `<div class="thumbtext p-1 absolute rounded-md text-xs bg-neutral-900">${pubtime}</div>`
-
     }
     else if (hour > 0) {
         const pubtime = `${hour}hrs ${minute} min ago`;
         return `<div class="thumbtext p-1 absolute rounded-md text-xs bg-neutral-900">${pubtime}</div>`
-
     }
     else if (minute > 0) {
         const pubtime = `${minute} min ago`;
@@ -104,18 +94,7 @@ function convertsecondtodhm(seconds) {
     }
 }
 
-
 function categorybutton(catid) {
     loadpage(catid);
 }
 loadpage('1000');
-
-
-
-
-
-// category : 'https://openapi.programming-hero.com/api/videos/categories'
-
-// no data found message : 'https://openapi.programming-hero.com/api/videos/category/$%7Bid%7D'
-
-// 
